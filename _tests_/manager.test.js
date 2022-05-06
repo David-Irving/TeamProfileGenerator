e3c3fa9d590ea7f828d12013094904f782ea03e6
David-Irving <djirving84@gmail.com>
2022-05-05 19:47:56 -0600

    manager test

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,16 @@
-// I do not understand tests AT ALL
+const Engineer = require("../lib/manager");
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("ManagerName", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "manager";
+  const e = new Engineer("Manager", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("ManagerName", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
